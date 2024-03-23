9da6b262b274b1d55e8f5ac00fb17d19e1190815
Yakumo-zi <1477717283@qq.com>
2024-03-23 20:33:27 +0800

    feat:基础组件结构

---
+++ src/Layout/NavBar.vue
@@ -0,0 +1,17 @@
+<script setup>
+
+</script>
+
+<template>
+    <div class="container">
+
+    </div>
+</template>
+
+<style scoped>
+.container {
+    width: 100%;
+    height: 50px;
+    background-color: #333333;
+}
+</style>
